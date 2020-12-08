80288fad731a10d84f8bdfe4223f6700b14d0337
udaibirv <udaibirv@gmail.com>
2020-12-07 18:36:38 -0800

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML.#2

--- js/data.js
+++ js/data.js
@@ -12,13 +12,14 @@ var data = {
   entries: []
 
 };
+var jsonData = JSON.parse(localStorage.getItem('profileData'));
+console.log(jsonData);
+if (jsonData !== null) {
+  data = jsonData;
+}
 
 window.addEventListener('beforeunload', function (event) {
   var stringData = JSON.stringify(data);
   localStorage.setItem('profileData', stringData);
 
 });
-var jsonData = JSON.parse(localStorage.getItem('profileData'));
-if (jsonData !== null) {
-  jsonData = data;
-}
--- js/main.js
+++ js/main.js
@@ -17,3 +17,48 @@ function handleSubmit(event) {
   $form.reset();
 
 }
+function profileTree(data) {
+
+  var $div = document.createElement('div');
+  $div.setAttribute('class', 'container');
+
+  var $row = document.createElement('div');
+  $row.setAttribute('class', 'row');
+  $div.appendChild($row);
+
+  var $columnFull = document.createElement('div');
+  $columnFull.setAttribute('class', 'column-full');
+  $row.appendChild($columnFull);
+
+  var $h1 = document.createElement('h1');
+  $h1.setAttribute('class', 'header-edit');
+  $h1.textContent = data.profile.fullName;
+  $columnFull.appendChild($h1);
+
+  var $img = document.createElement('img');
+  $img.setAttribute('src', data.profile.avatarUrl);
+  $columnFull.appendChild($img);
+
+  var $i = document.createElement('i');
+  $i.setAttribute('class', 'fas fa-book-reader');
+  $i.textContent = data.profile.username;
+  $columnFull.appendChild($i);
+
+  var $iTwo = document.createElement('i');
+  $iTwo.setAttribute('class', 'fas fa-anchor');
+  $iTwo.textContent = data.profile.location;
+  $columnFull.appendChild($iTwo);
+
+  var $columnHalf = document.createElement('div');
+  $columnHalf.setAttribute('class', 'column-third');
+  $row.appendChild($columnHalf);
+
+  var $bio = document.createElement('div');
+  $bio.setAttribute('class', 'edit-bio');
+  $bio.textContent = data.profile.bio;
+  $columnHalf.appendChild($bio);
+
+
+  return $div;
+
+}
